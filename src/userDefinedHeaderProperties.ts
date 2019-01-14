20eefb57cbfed3befa3a8e11352af60e3d98d4ee
Kazuyuki Sato <kaz>
2019-01-14 23:23:07 +0900

    * Adopt [bpostlethwaite/colormap](https://github.com/bpostlethwaite/colormap) * Add user defined settings for each header level

---
+++ src/userDefinedHeaderProperties.ts
@@ -0,0 +1,44 @@
+export interface userDefinedHeaderProperties {
+    enabled?: Boolean;
+    Header_1?: {
+        color?: string;
+        backgroundColor?: string;
+        textDecoration?: string;
+		overviewRulerColor?: string;
+    };
+    Header_2?: {
+        color?: string;
+        backgroundColor?: string;
+        textDecoration?: string;
+		overviewRulerColor?: string;
+    };
+    Header_3?: {
+        color?: string;
+        backgroundColor?: string;
+        textDecoration?: string;
+		overviewRulerColor?: string;
+    };
+    Header_4?: {
+        color?: string;
+        backgroundColor?: string;
+        textDecoration?: string;
+		overviewRulerColor?: string;
+    };
+    Header_5?: {
+        color?: string;
+        backgroundColor?: string;
+        textDecoration?: string;
+		overviewRulerColor?: string;
+    };
+    Header_6?: {
+        color?: string;
+        backgroundColor?: string;
+        textDecoration?: string;
+		overviewRulerColor?: string;
+    };
+}
+
+export interface userDefinedColormapProperties {
+    colormap?: string;
+    nshades?: number;
+}
